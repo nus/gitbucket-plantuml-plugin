7cd3b415c3fdbeae79aff702310819d73ac675f7
Yota Ichino <www.irda@gmail.com>
2017-03-12 17:59:50 +0900

    Render with a URL of plantuml image #1

--- src/main/scala/com/yotaichino/gitbucket/plugins/plantuml/PlantUMLRenderer.scala
+++ src/main/scala/com/yotaichino/gitbucket/plugins/plantuml/PlantUMLRenderer.scala
@@ -2,18 +2,22 @@ package com.yotaichino.gitbucket.plugins.plantuml
 
 import gitbucket.core.plugin.Renderer
 import gitbucket.core.plugin.RenderRequest
+import gitbucket.core.util.StringUtil
 import java.nio.charset.Charset
 import play.twirl.api.Html
 
 class PlantUMLRenderer extends Renderer {
 
   override def render(request: RenderRequest): Html = {
-    Html(toHtml(request.fileContent))
+    val owner = request.repository.owner
+    val name = request.repository.name
+    val branch = request.branch
+    val filePath = request.filePath.mkString("/")
+    Html(toHtml(owner, name, branch, filePath))
   }
 
-  def toHtml(fileContent: String): String = {
-    val svg = new String(PlantUMLUtils.generateSVGImage(fileContent), Charset.forName("UTF-8"));
-
-    """<object type="image/svg+xml" data="plantuml.svg">""" + svg + "</object>"
+  def toHtml(owner: String, name: String, branch: String, filePath: String): String = {
+    val pumlPath = StringUtil.escapeHtml(s"/$owner/$name/plantuml/$branch/$filePath")
+    s"""<img src="$pumlPath">"""
   }
 }
