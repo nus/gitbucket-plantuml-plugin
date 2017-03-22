d53344b8de70099594128cef9cb0ad604f4b3393
Yota Ichino <www.irda@gmail.com>
2017-03-23 00:19:27 +0900

    Implement preview request

--- src/main/scala/com/yotaichino/gitbucket/plugins/plantuml/PlantUMLRenderer.scala
+++ src/main/scala/com/yotaichino/gitbucket/plugins/plantuml/PlantUMLRenderer.scala
@@ -4,6 +4,7 @@ import gitbucket.core.plugin.Renderer
 import gitbucket.core.plugin.RenderRequest
 import gitbucket.core.util.StringUtil
 import java.nio.charset.Charset
+import java.util.Base64
 import play.twirl.api.Html
 
 class PlantUMLRenderer extends Renderer {
@@ -11,12 +12,24 @@ class PlantUMLRenderer extends Renderer {
   override def render(request: RenderRequest): Html = {
     val owner = request.repository.owner
     val name = request.repository.name
-    val branch = request.branch
-    val filePath = request.filePath.mkString("/")
-    Html(toHtml(owner, name, branch, filePath))
+    val preview = s"/$owner/$name/_preview".equals(request.context.currentPath)
+
+    if (preview) {
+      Html(imgEmbedded(request.fileContent))
+    } else {
+      val branch = request.branch
+      val filePath = request.filePath.mkString("/")
+      Html(imgLink(owner, name, branch, filePath))
+    }
   }
 
-  def toHtml(owner: String, name: String, branch: String, filePath: String): String = {
+  def imgEmbedded(content: String): String = {
+    val raw = PlantUMLUtils.generateSVGImage(content)
+    val src = Base64.getEncoder.encodeToString(raw)
+    s"""<img src="data:image/svg+xml;base64,$src">"""
+  }
+
+  def imgLink(owner: String, name: String, branch: String, filePath: String): String = {
     val pumlPath = StringUtil.escapeHtml(s"/$owner/$name/plantuml/$branch/$filePath")
     s"""<img src="$pumlPath">"""
   }
