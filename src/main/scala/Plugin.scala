b0bfe49b5eebb88cd6f501b416ddf2bfc1824661
Yota Ichino <www.irda@gmail.com>
2017-06-11 17:17:20 +0900

    Update version 1.2.1

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -14,7 +14,8 @@ class Plugin extends gitbucket.core.plugin.Plugin {
     new Version("1.1.0"),
     new Version("1.1.1"),
     new Version("1.1.2"),
-    new Version("1.2.0")
+    new Version("1.2.0"),
+    new Version("1.2.1")
   )
 
   override def initialize(registry: PluginRegistry, context: ServletContext, settings: SystemSettings): Unit = {
